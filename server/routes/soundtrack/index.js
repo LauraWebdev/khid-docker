ae6c07e5c0dcb36d5d9a630cbd062e7b48b9a6da
Laura Sofia Heimann <hello@laura.media>
2025-01-17 11:34:41 +0100

    Initial work for cover retrieval

--- server/routes/soundtrack/index.js
+++ server/routes/soundtrack/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {getSoundtrackMeta} = require("../../modules/getSoundtrackMeta");
+const {getSoundtrackCover} = require("../../modules/getSoundtrackCover");
 const Router = express.Router;
 
 const router = new Router();
@@ -23,6 +24,22 @@ router.post('/', async (req, res) => {
     res.json({
         status: 200,
         data: soundtrackMeta
+    });
+});
+
+router.post('/cover', async (req, res) => {
+    if (req.body.urlOrSlug === undefined) {
+        return res.json({
+            status: 400,
+            message: '`urlOrSlug` is required'
+        });
+    }
+
+    let soundtrackCover = await getSoundtrackCover(req.body.urlOrSlug);
+
+    res.json({
+        status: 200,
+        data: soundtrackCover
     });
 });
 
